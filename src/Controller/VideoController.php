693abe4b4412d0f096d45af69f6b71bd6fffe820
Joseochoa227 <joseochoa227@gmail.com>
2019-05-17 11:46:47 -0400

    Controlador de video creado y modificacion de rutas en el archivo routes.yaml

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
+class VideoController extends AbstractController
 {
-    
+    /**
+     * @Route("/video", name="video")
+     */
     public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
+            'path' => 'src/Controller/VideoController.php',
         ]);
     }
 }
