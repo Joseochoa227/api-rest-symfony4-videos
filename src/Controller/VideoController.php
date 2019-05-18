2e7027cd7e233ad1a1f142ddcc130b30c15016b9
Joseochoa227 <joseochoa227@gmail.com>
2019-05-18 14:16:58 -0400

    Primer commit desde la rama de videos

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -4,17 +4,39 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\Email;
 
+
+use App\Entity\User;
+use App\Entity\Video;
+use App\Services\JwtAuth;
 class VideoController extends AbstractController
 {
-    /**
-     * @Route("/video", name="video")
-     */
+    private function resjson($data){
+        //Conseguir el servicio de serializacion (Serializar datos)
+        $json = $this->get('serializer')->serialize($data, 'json');
+        //Response con HTTP fundation
+        $response = new Response();
+        //Asginar contenido a la respuesta
+        $response->setContent($json);
+        //Indicar formato de respuesta
+        $response->headers->set('Content-Type', 'application/json');
+        //Devolver la respuesta
+        return $response;
+    }
     public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/VideoController.php',
         ]);
+    }
+
+    public function newVideo(Request $request, JwtAuth $jwt_auth){
+
     }
 }
