54d68485b360ef6b0dee231fe9e2b349df43762b
Joseochoa227 <joseochoa227@gmail.com>
2019-05-18 15:02:39 -0400

    Terminado el metodo para agregar videos favoritos a la base de datos

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -37,24 +37,69 @@ class VideoController extends AbstractController
     }
 
     public function create(Request $request, JwtAuth $jwt_auth){
-        //Recoger el token
-
-        //Comprobar si es correcto
-
-        //Recoger datos por POST
-
-        //Recoger el objeto de usuario identificado
-
-        //Comprobar y validar datos
-
-        //Guardar el nuevo video favorito en la base de datos
-
-        //Devolver respuesta
+        //datos por defecto
         $data = [
             'status' => 'error',
             'code'   =>  400,
             'message'=> 'El video no ha podido crearse'
         ];
+        
+        //Recoger el token
+        $token = $request->headers->get('Authorization');
+        //Comprobar si es correcto
+        $authCheck = $jwt_auth->checkToken($token);
+
+        if($authCheck){
+            //Recoger datos por POST
+            $json = $request->get('json', null);
+            $params = json_decode($json);
+
+             //Recoger el objeto de usuario identificado
+             $identity = $jwt_auth->checkToken($token, true);
+
+            //Comprobar y validar datos
+            if(!empty($json)){
+                $user_id      = ($identity->sub != null) ? $identity->sub : null;
+                $title        = (!empty($params->title)) ? $params->title : null;
+                $description  = (!empty($params->description)) ? $params->description : null;
+                $url          =(!empty($params->url)) ? $params->url : null;
+
+                if(!empty($user_id) && !empty($title)){
+                    //Guardar el nuevo video favorito en la base de datos
+                    $em = $this->getDoctrine()->getManager();
+                    $user = $this->getDoctrine()->getRepository(User::class)->findOneBy([
+                        'id'    => $user_id
+                    ]);
+                    // crear y guardar objeto
+
+                    $video = new Video();
+                    $video->setUser($user);
+                    $video->setTitle($title);
+                    $video->setDescription($description);
+                    $video->setUrl($url);
+                    $video->setStatus('Normal');
+                    
+                    $createdAt = new \Datetime('now');
+                    $updatedAt = new \Datetime('now');
+
+                    $video->setCreatedAt($createdAt);
+                    $video->setUpdatedAt($updatedAt);
+
+                    //Guardar en BD
+
+                    $em->persist($video);
+                    $em->flush();
+                    $data = [
+                        'status' => 'success',
+                        'code'   =>  200,
+                        'message'=> 'El video se ha guardado',
+                        'video'  => $video
+                    ];
+                }
+            }
+        }
+        //Devolver respuesta
+       
         return $this->resjson($data);
     }
 }
